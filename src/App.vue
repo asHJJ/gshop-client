dec3a4b64372bee2eaad5ccfd8f281989983079b
asHJJ <934606664@qq.com>
2018-12-21 21:21:06 +0800

    vue实战项目-各导航路由组件(静态)

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script src="main.js"></script>
+<template>
+  <div>
+    <router-view/>
+    <FooterGuide/>
+  </div>
+</template>
+
+<script>
+    import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+    export default {
+        components: {
+            FooterGuide
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
